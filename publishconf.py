f6a3e52f9bdc8ba8f880bfa6f4d3cf02484b59f3
Michael Haslam <hreikin@gmail.com>
2023-02-04 15:58:00 +0000

    Add author atom feed.

--- publishconf.py
+++ publishconf.py
@@ -12,6 +12,7 @@ RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+AUTHOR_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
